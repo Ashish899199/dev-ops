f3d1359d3f995a31a1b239ea9f324aa7bd90d8e2
Ashish899199 <48611536+Ashish899199@users.noreply.github.com>
2026-01-10 15:56:38 +0530

    Delete bookms directory

--- bookms/src/main/java/com/library/bookms/repository/BookRepository.java
+++
@@ -1,9 +0,0 @@
-package com.library.bookms.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.library.bookms.entity.Book;
-
-public interface BookRepository extends JpaRepository<Book,Long> {
-
-}
